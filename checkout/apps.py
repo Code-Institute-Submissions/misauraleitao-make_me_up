f5707bbd619bae89d8b862e5bc945c27c61420b4
misauraleitao <misauraleitao@gmail.com>
2022-01-26 13:02:59 +0000

    created forms.py, signals.py and added to admin.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        '''Overriding the ready method and 
+        importing our signals module.'''
+        import checkout.signals
