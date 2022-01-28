d15cecc66e96290ef3fddc6cba50c85e54592a90
misauraleitao <misauraleitao@gmail.com>
2022-01-28 17:35:35 +0000

    created templates, views and urls for profile page

--- make_me_up/settings.py
+++ make_me_up/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    'profiles'
+    'profile',
 
     # Other
     'crispy_forms',
@@ -189,3 +189,4 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
