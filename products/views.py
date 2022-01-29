a95cb74b08f15d590d6bef6f9da9b26033e7d703
misauraleitao <misauraleitao@gmail.com>
2022-01-29 13:07:31 +0000

    product edit and delete functionality updated and completed alongside with ther respective links on the templates

--- products/views.py
+++ products/views.py
@@ -71,13 +71,13 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """ Add a product to the store """
+    """ Add a product """
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Good to go! You have added a new FIRE product!!!!')
-            return redirect(reverse('add_product'))
+            product = form.save()
+            messages.success(request, 'Wooo new FIRE Product added!')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Oops! Product failed to add to database. Please ensure the form is valid.')
     else:
@@ -92,7 +92,7 @@ def add_product(request):
 
 
 def edit_product(request, product_id):
-    """ Edit a product in the store """
+    """ Edit a product """
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -113,3 +113,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
