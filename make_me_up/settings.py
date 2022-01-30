ba0e0b41d34e9257b4a32da69993fa41a89d9ed7
misauraleitao <misauraleitao@gmail.com>
2022-01-30 13:50:37 +0000

    added cache control

--- make_me_up/settings.py
+++ make_me_up/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '5z$1-f32p8bjv580ngcm*s=_p0inhg6n^w9xnsvx)6=chrk%%w')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -189,6 +189,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket aws config
     AWS_STORAGE_BUCKET_NAME = "makemeupbymaria"
     AWS_S3_REGION_NAME = "eu-west-2"
